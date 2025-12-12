88218a7e1a629698d9c457d321fb5b3e2eb2f5fa
Paco <camerobarrosofrancisco@gmail.com>
2025-12-12 14:08:05 +0100

    subida CRUD

--- src/main/java/com/jacaranda/controller/PersonController.java
+++ src/main/java/com/jacaranda/controller/PersonController.java
@@ -1,9 +1,16 @@
 package com.jacaranda.controller;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.jacaranda.model.Person;
 import com.jacaranda.services.PersonService;
 
 @Controller
@@ -18,7 +25,122 @@ public class PersonController {
 	
 	@GetMapping({"/","persons"})
 	public String getPersons(Model model) {
-		model.addAttribute("listPerson",personService.getPersons());
+		try {
+			model.addAttribute("listPerson",personService.getPersons());
+		} catch (Exception e) {
+			model.addAttribute("error",e.getMessage());
+
+		}
 		return "listPerson";
 	}
+	
+	@GetMapping("/viewPerson")
+	public String getPersonById(Model model,@ModelAttribute Person p, @RequestParam Optional<Integer> id,@RequestParam Optional<String> action) {
+		try {
+			p = personService.getPersonById(id.orElse(null));
+			
+			model.addAttribute("person", p);
+			model.addAttribute("action", action.orElse(null));
+
+		} catch (Exception e) {
+			model.addAttribute("error",e.getMessage());
+
+		}
+		return "formPerson";
+	}
+	
+	@GetMapping("/deletePerson")
+	public String deletePersonGet(Model model,@ModelAttribute Person p, @RequestParam Optional<Integer> id,@RequestParam Optional<String> action) {
+		try {
+			
+			p = personService.getPersonById(id.orElse(null));
+			model.addAttribute("action", action.orElse(null));
+			model.addAttribute("person", p);
+		} catch (Exception e) {
+			model.addAttribute("error",e.getMessage());
+
+		}
+		return "formPerson";
+	}
+	
+	@PostMapping("/deletePerson")
+	public String deletePersonPost(Model model,@ModelAttribute Person p) {
+		try {
+			
+			personService.deletePerson(p);
+			model.addAttribute("person", p);
+			model.addAttribute("listPerson",personService.getPersons());
+
+		} catch (Exception e) {
+			model.addAttribute("error",e.getMessage());
+
+		}
+		return "listPerson";
+	}
+	
+	@GetMapping("/editPerson")
+	public String editPersonGet(Model model,@ModelAttribute Person p, @RequestParam Optional<Integer> id,@RequestParam Optional<String> action) {
+		try {
+			
+			p = personService.getPersonById(id.orElse(null));
+			model.addAttribute("action", action.orElse(null));
+			model.addAttribute("person", p);
+		} catch (Exception e) {
+			model.addAttribute("error",e.getMessage());
+
+		}
+		return "formPerson";
+	}
+	
+	@PostMapping("/editPerson")
+	public String editPersonPost(Model model,@ModelAttribute Person p) {
+		try {
+			
+			personService.editPerson(p);
+			model.addAttribute("person", p);
+			model.addAttribute("listPerson",personService.getPersons());
+
+		} catch (Exception e) {
+			model.addAttribute("error",e.getMessage());
+
+		}
+		return "listPerson";
+	}
+	
+	@GetMapping("/addPerson")
+	public String addPersonGet(Model model,@RequestParam Optional<String> action) {
+		try {
+			
+			model.addAttribute("action", action.orElse(null));
+			model.addAttribute("person", new Person());
+		} catch (Exception e) {
+			model.addAttribute("error",e.getMessage());
+
+		}
+		return "formPerson";
+	}
+	
+	@PostMapping("/addPerson")
+	public String addPersonPost(Model model,@ModelAttribute Person p,@RequestParam Optional<String> action) {
+		try {
+			
+			personService.addPerson(p);
+			model.addAttribute("person", p);
+			model.addAttribute("listPerson",personService.getPersons());
+
+		} catch (Exception e) {
+			model.addAttribute("error",e.getMessage());
+			model.addAttribute("action", action.orElse(null));
+
+			return "formPerson";
+
+		}
+		return "listPerson";
+	}
+	
+	
+	
+	
+	
+	
 }
--- src/main/java/com/jacaranda/services/PersonService.java
+++ src/main/java/com/jacaranda/services/PersonService.java
@@ -1,6 +1,7 @@
 package com.jacaranda.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -16,7 +17,57 @@ public class PersonService {
 		this.personRepository = personRepository;
 	}
 	
-	public List<Person> getPersons(){
-		return this.personRepository.findAll();
+	//Listar las personas
+	public List<Person> getPersons() throws Exception{
+		try {
+			return this.personRepository.findAll();
+		}catch(Exception e) {
+			throw new Exception("Error en listar personas");
+		}
+		
+	}
+	
+	//Ver persona por id
+	public Person getPersonById(Integer id) throws Exception {
+		try {
+			return this.personRepository.findById(id).orElse(null);
+		}catch(Exception e) {
+			throw new Exception("Error en ver detalles de la persona");
+
+		}
+	}
+	
+	//Editar persona
+
+		public void editPerson(Person newPer) throws Exception {
+			try {
+				Person p = this.personRepository.findById(newPer.getId()).orElse(null);
+				if(p!=null) {
+					p = newPer;
+					this.personRepository.save(p);
+				}
+			}catch(Exception e) {
+				throw new Exception("Error en editar la persona");
+
+			}
+		}
+	
+		//Eliminar persona
+	public void deletePerson(Person p) throws Exception {
+		try {
+			this.personRepository.delete(p);
+		}catch(Exception e) {
+			throw new Exception("Error en eliminar la persona");
+
+		}
+	}
+	//Añadir persona
+	public void addPerson(Person p) throws Exception {
+		try {
+			this.personRepository.save(p);
+		}catch(Exception e) {
+			throw new Exception("Error en añadir la persona");
+
+		}
 	}
 }
